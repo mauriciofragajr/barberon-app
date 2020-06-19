fe29a9b8bdaea4076d42a899338dd149f9a5ae8e
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2020-06-19 05:10:05 -0300

    trocando nome do app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app_flutter/main.dart';
+import 'package:barberOn/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
