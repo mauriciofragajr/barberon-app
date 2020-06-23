5706991a02c241f07777e0e4100658acbec12745
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2020-06-22 23:45:26 -0300

    incluindo imagem no projeto

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key, this.toggleView}) : super(key: key);
-  
+
   final Function toggleView;
-  
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -21,90 +21,101 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-                validator: (input) {
-                  if (input.isEmpty) {
-                    return 'Por favor, digite um e-mail';
-                  }
-                  return null;
-                },
-                onSaved: (input) => {_email = input},
-                decoration: InputDecoration(labelText: 'E-mail')),
-            TextFormField(
-              validator: (input) {
-                if (input.isEmpty) {
-                  return 'Por favor, digite uma senha';
-                }
-                if (input.length < 6) {
-                  return 'Sua senha deve conter no mínimo 6 caracteres';
-                }
-                return null;
-              },
-              onSaved: (input) => {_password = input},
-              decoration: InputDecoration(labelText: 'Senha'),
-              obscureText: true,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return loading
+        ? Loading()
+        : Scaffold(
+            body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
               children: <Widget>[
-                RaisedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      _formKey.currentState.save();
-                      setState(() {
-                        loading = true;
-                        error = '';
-                      });
-                      dynamic result = await _auth.signInWithEmailAndPassword(
-                          _email, _password);
-                      if (result == null) {
-                        setState(() {
-                          loading = false;
-                          error = 'Usuário ou senha inválida';
-                        });
-                      }
-                    }
-                  },
-                  child: Text('Entrar'),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 40.0),
+                  child: Text('Barberon'),
                 ),
-                RaisedButton(
-                  onPressed: () async {
-                    setState(() {
-                      loading = true;
-                      error = '';
-                    });
-                    dynamic result = await _auth.signInWithGoogle();
-                    if (result == null) {
-                      setState(() {
-                        loading = false;
-                        error = 'Usuário ou senha inválida';
-                      });
-                    }
-                  },
-                  child: Text('Login com google'),
-                ),
-                RaisedButton(
-                  onPressed: () => widget.toggleView(),
-                  child: Text('Cadastrar'),
-                ),
-                SizedBox(height: 12.0),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.white, fontSize: 14.0),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                          validator: (input) {
+                            if (input.isEmpty) {
+                              return 'Por favor, digite um e-mail';
+                            }
+                            return null;
+                          },
+                          onSaved: (input) => {_email = input},
+                          decoration: InputDecoration(labelText: 'E-mail')),
+                      TextFormField(
+                        validator: (input) {
+                          if (input.isEmpty) {
+                            return 'Por favor, digite uma senha';
+                          }
+                          if (input.length < 6) {
+                            return 'Sua senha deve conter no mínimo 6 caracteres';
+                          }
+                          return null;
+                        },
+                        onSaved: (input) => {_password = input},
+                        decoration: InputDecoration(labelText: 'Senha'),
+                        obscureText: true,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          RaisedButton(
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                _formKey.currentState.save();
+                                setState(() {
+                                  loading = true;
+                                  error = '';
+                                });
+                                dynamic result =
+                                    await _auth.signInWithEmailAndPassword(
+                                        _email, _password);
+                                if (result == null) {
+                                  setState(() {
+                                    loading = false;
+                                    error = 'Usuário ou senha inválida';
+                                  });
+                                }
+                              }
+                            },
+                            child: Text('Entrar'),
+                          ),
+                          RaisedButton(
+                            onPressed: () async {
+                              setState(() {
+                                loading = true;
+                                error = '';
+                              });
+                              dynamic result = await _auth.signInWithGoogle();
+                              if (result == null) {
+                                setState(() {
+                                  loading = false;
+                                  error = 'Usuário ou senha inválida';
+                                });
+                              }
+                            },
+                            child: Text('Login com google'),
+                          ),
+                          RaisedButton(
+                            onPressed: () => widget.toggleView(),
+                            child: Text('Cadastrar'),
+                          ),
+                          SizedBox(height: 12.0),
+                          Text(
+                            error,
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 14.0),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 )
               ],
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          ));
   }
 }
