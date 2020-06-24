eb167678ad362f9b755a4767cb5c2e573ed25631
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2020-06-23 23:04:44 -0300

    padding na rela de cadastro

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -24,83 +24,86 @@ class _RegisterPageState extends State<RegisterPage> {
       appBar: AppBar(
         title: Text('Register'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                  validator: (input) {
+                    if (input.isEmpty) {
+                      return 'Por favor, digite um e-mail';
+                    }
+                    return null;
+                  },
+                  onSaved: (input) => {_email = input},
+                  decoration: InputDecoration(labelText: 'E-mail')),
+              TextFormField(
                 validator: (input) {
                   if (input.isEmpty) {
-                    return 'Por favor, digite um e-mail';
+                    return 'Por favor, digite uma senha';
+                  }
+                  if (input.length < 6) {
+                    return 'Sua senha deve conter no mínimo 6 caracteres';
                   }
                   return null;
                 },
-                onSaved: (input) => {_email = input},
-                decoration: InputDecoration(labelText: 'E-mail')),
-            TextFormField(
-              validator: (input) {
-                if (input.isEmpty) {
-                  return 'Por favor, digite uma senha';
-                }
-                if (input.length < 6) {
-                  return 'Sua senha deve conter no mínimo 6 caracteres';
-                }
-                return null;
-              },
-              onSaved: (input) => {_password = input},
-              decoration: InputDecoration(labelText: 'Senha'),
-              obscureText: true,
-            ),
-            TextFormField(
-              validator: (input) {
-                if (input.isEmpty) {
-                  return 'Por favor, digite uma senha';
-                }
-                if (input.length < 6) {
-                  return 'Sua senha deve conter no mínimo 6 caracteres';
-                }
-                return null;
-              },
-              onSaved: (input) => {_confirmPassword = input},
-              decoration: InputDecoration(labelText: 'Confirmar senha'),
-              obscureText: true,
-            ),
-            RaisedButton(
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
-                  if (_password != _confirmPassword) {
-                    setState(() {
-                      error = 'Senhas não coincidem';
-                    });
-                    return;
+                onSaved: (input) => {_password = input},
+                decoration: InputDecoration(labelText: 'Senha'),
+                obscureText: true,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Por favor, digite uma senha';
                   }
-                  setState(() {
-                    loading = true;
-                    error = '';
-                  });
-                  dynamic result = await _auth.registerWithEmailAndPassword(
-                      _email, _password);
-                  if (result != null) {
-                    setState(() {
-                      loading = false;
-                      error = result;
-                    });
+                  if (input.length < 6) {
+                    return 'Sua senha deve conter no mínimo 6 caracteres';
                   }
-                }
-              },
-              child: Text('Cadastrar'),
-            ),
-            RaisedButton(
-              onPressed: () => widget.toggleView(),
-              child: Text('Voltar'),
-            ),
-            SizedBox(height: 12.0),
-            Text(
-              error,
-              style: TextStyle(color: Colors.white, fontSize: 14.0),
-            )
-          ],
+                  return null;
+                },
+                onSaved: (input) => {_confirmPassword = input},
+                decoration: InputDecoration(labelText: 'Confirmar senha'),
+                obscureText: true,
+              ),
+              RaisedButton(
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    if (_password != _confirmPassword) {
+                      setState(() {
+                        error = 'Senhas não coincidem';
+                      });
+                      return;
+                    }
+                    setState(() {
+                      loading = true;
+                      error = '';
+                    });
+                    dynamic result = await _auth.registerWithEmailAndPassword(
+                        _email, _password);
+                    if (result != null) {
+                      setState(() {
+                        loading = false;
+                        error = result;
+                      });
+                    }
+                  }
+                },
+                child: Text('Cadastrar'),
+              ),
+              RaisedButton(
+                onPressed: () => widget.toggleView(),
+                child: Text('Voltar'),
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.white, fontSize: 14.0),
+              )
+            ],
+          ),
         ),
       ),
     );
